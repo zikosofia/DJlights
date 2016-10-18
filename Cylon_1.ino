47166ffa7df3359f00937fd076499a4ea299b7d5
zikosofia <zaid@utexas.edu>
2016-10-17 21:26:19 -0500

    touchpad

---
+++ Cylon_1.ino
@@ -0,0 +1,89 @@
+#include "FastLED.h"
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_TFTLCD.h> // Hardware-specific library
+#include <TouchScreen.h>
+
+#if defined(__SAM3X8E__)
+    #undef __FlashStringHelper::F(string_literal)
+    #define F(string_literal) string_literal
+#endif
+
+// How many leds in your strip?
+#define NUM_LEDS 100 
+
+// For led chips like Neopixels, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806, define both DATA_PIN and CLOCK_PIN
+#define DATA_PIN 13
+#define CLOCK_PIN 7
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+#define YP A3  // must be an analog pin, use "An" notation!
+#define XM A2  // must be an analog pin, use "An" notation!
+#define YM 9   // can be a digital pin
+#define XP 8   // can be a digital pin
+
+#define TS_MINX 200
+#define TS_MINY 200
+#define TS_MAXX 920
+#define TS_MAXY 900
+
+// For better pressure precision, we need to know the resistance
+// between X+ and X- Use any multimeter to read it
+// For the one we're using, its 300 ohms across the X plate
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+
+#define LCD_CS A3
+#define LCD_CD A2
+#define LCD_WR A1
+#define LCD_RD A0
+// optional
+#define LCD_RESET A4
+
+// Assign human-readable names to some common 16-bit color values:
+#define  BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
+
+void setup() { 
+	Serial.begin(57600);
+	Serial.println("resetting");
+	LEDS.addLeds<WS2812,DATA_PIN,RGB>(leds,NUM_LEDS);
+	LEDS.setBrightness(84);
+}
+
+void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
+
+void loop() { 
+	tft.reset();
+	static uint8_t hue = 0;
+  int LEDrow =0 ;
+  int LEDcolumn = 0;
+  int i =0;
+  digitalWrite(13, HIGH);
+  TSPoint p = ts.getPoint();
+  digitalWrite(13, LOW);
+  if(p.z > 400)
+  {    
+    LEDrow = p.x/90;
+    LEDcolumn = p.y/80;
+
+    leds[((LEDrow*10)-10) + LEDcolumn] = CHSV(120, 255, 255);
+    FastLED.show();
+    delay(10);
+
+    leds[((LEDrow*10)-10) + LEDcolumn] = CHSV(0, 0, 0);
+    FastLED.show();
+  }
+}
